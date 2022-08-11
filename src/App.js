03e6571b92acdffe9e195a71f4e060a16d2bf2de
claudeando <claude.mm6@gmail.com>
2022-08-11 11:09:40 +0100

    -

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ import {
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-const TRACKING_ID = "G-23SXWNFYHN"; // YOUR_OWN_TRACKING_ID
+// const TRACKING_ID = "G-23SXWNFYHN"; // YOUR_OWN_TRACKING_ID
 
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname);
+// ReactGA.initialize(TRACKING_ID);
+// ReactGA.pageview(window.location.pathname);
 
 
 
