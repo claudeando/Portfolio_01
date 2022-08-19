7871657078dbeb7fecf454e846153ee667b5a169
claudeando <claude.mm6@gmail.com>
2022-08-19 18:34:55 +0100

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import './index.css';
