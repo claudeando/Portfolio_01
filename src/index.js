5e66537e2a88afa0d3bdd10a8c147f1b6031c719
claudeando <claude.mm6@gmail.com>
2022-08-19 19:16:11 +0100

    ga test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
 import ReactGA from 'react-ga';
+import App from './App';
 
 const TRACKING_ID = "G-23SXWNFYHN"; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
