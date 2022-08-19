99d28abb4163cd65d92de1581b7270bfb6674d84
claudeando <claude.mm6@gmail.com>
2022-08-19 18:49:00 +0100

    react-ga

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// import ReactGA from 'react-ga';
+import ReactGA from 'react-ga';
 
-// const TRACKING_ID = "G-23SXWNFYHN"; // YOUR_OWN_TRACKING_ID
+const TRACKING_ID = "G-23SXWNFYHN"; // YOUR_OWN_TRACKING_ID
 
-// ReactGA.initialize(TRACKING_ID);
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
