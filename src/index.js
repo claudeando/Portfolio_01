7153815451131f6781a414ec81885dcc646e758e
claudeando <claude.mm6@gmail.com>
2022-11-18 17:23:47 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import ReactGA from 'react-ga';
 import App from './App';
-
-const TRACKING_ID = "G-23SXWNFYHN"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
